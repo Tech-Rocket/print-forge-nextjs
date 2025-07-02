8eabf758462a675d5ce6c204fb83ba107b965904
Tech-Rocket <beelawrence8@gmail.com>
2025-07-02 16:57:56 +0000

    Add mobile logo for PrintForge in PNG format

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
+import heroImg from "@/public/images/Frame4.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      <h1>Home paged</h1>
+    <section className="py-[2rem]">
+      <div className="space-y-5 px-3">
+        <h1 className="text-5xl font-bold">
+          Discover what’s possible with 3D printing
+        </h1>
+        <p className="text-xl font-medium text-gray-500">
+          Join our community of creators and explore a vast library of
+          user-submitted models.
+        </p>
+        <button className="uppercase px-6 py-2.5 border-2 border-black cursor-pointer font-semibold">
+          browse models
+        </button>
+      </div>
+
+      <Image
+        src={heroImg}
+        alt="hero image"
+        width={3000}
+        height={3000}
+        className="w-full h-[23rem] mt-6 mb-2"
+      />
     </section>
   );
 }
---
+++ components/shared/header.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import printLogo from "@/public/images/printforge-logomobile.png";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="h-[3.5rem] flex items-center justify-between px-3">
+      <Link href="#">
+        <Image
+          className="h-10 w-10 cursor-pointer"
+          src={printLogo}
+          alt="print logo"
+          width={3000}
+          height={3000}
+        />
+      </Link>
+      <ul className="flex gap-5 uppercase text-slate-600">
+        <li className="cursor-pointer">
+          <Link href="#">3d model</Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link href="#">About</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
